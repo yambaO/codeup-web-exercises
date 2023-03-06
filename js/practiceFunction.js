eb39c442657434e18a36893935a0c239be8e54eb
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-03-06 11:45:55 -0600

    Adding forms exercises

--- js/practiceFunction.js
+++ js/practiceFunction.js
@@ -399,3 +399,175 @@ function totalPets(personArray){
  *        { name: "Martin",  age: 16, budget: 1600 }
  *    ]) ➞ 62600
  */
+
+//
+    // Make sure you are comfortable with these techniques: looping over the array,
+// console-logging the name of each city. Then try console-logging the name and state. Then try console-logging all the cities in Texas with populations over 1 million.  Write a function that accepts
+// one city object and returns true if the city is in Texas.  Write a function
+// that accepts an array of city objects and the name of a state and returns an array with only the cities in that state.
+
+
+const cities = [
+    {
+        name: "New York City",
+        state: "New York",
+        population: 8336817
+    },
+    {
+        name: "Los Angeles",
+        state: "California",
+        population: 3979576
+    },
+    {
+        name: "Chicago",
+        state: "Illinois",
+        population: 2693976
+    },
+    {
+        name: "Houston",
+        state: "Texas",
+        population: 2320268
+    },
+    {
+        name: "Phoenix",
+        state: "Arizona",
+        population: 1680992
+    },
+    {
+        name: "Philadelphia",
+        state: "Pennsylvania",
+        population: 1584064
+    },
+    {
+        name: "San Antonio",
+        state: "Texas",
+        population: 1547253
+    },
+    {
+        name: "San Diego",
+        state: "California",
+        population: 1423851
+    },
+    {
+        name: "Dallas",
+        state: "Texas",
+        population: 1343573
+    },
+    {
+        name: "San Jose",
+        state: "California",
+        population: 1030119
+    },
+    {
+        name: "Austin",
+        state: "Texas",
+        population: 964254
+    },
+    {
+        name: "Jacksonville",
+        state: "Florida",
+        population: 903889
+    },
+    {
+        name: "Fort Worth",
+        state: "Texas",
+        population: 895008
+    },
+    {
+        name: "Columbus",
+        state: "Ohio",
+        population: 892533
+    },
+    {
+        name: "San Francisco",
+        state: "California",
+        population: 883305
+    },
+    {
+        name: "Charlotte",
+        state: "North Carolina",
+        population: 885708
+    },
+    {
+        name: "Indianapolis",
+        state: "Indiana",
+        population: 876384
+    },
+    {
+        name: "Seattle",
+        state: "Washington",
+        population: 753675
+    },
+    {
+        name: "Denver",
+        state: "Colorado",
+        population: 727211
+    },
+    {
+        name: "Washington",
+        state: "District of Columbia",
+        population: 702455
+    }
+];
+for(let i = 0; i < cities.length; i++){
+    if (cities[i].state === "Texas" && cities[i].population > 1000000) {
+        console.log(`${cities[i].name}, ${cities[i].state}`);
+    }
+}
+// function isTexas(cityObject){
+//     if (cityObject.state === "texas"){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+// {
+//  const texas = {
+//      name: "San Antonio",
+//      state: "Texas",
+//      population: 1547253
+//  }
+// console.log(isTexas(texas));
+// cities.forEach(function (cities){
+//     let citiesNames = cities.name;
+//     console.log(citiesNames)
+// })
+// cities.forEach(function (cities ){
+//     let citiesNames = cities.name;
+//     let states = cities.state;
+//     console.log(`${citiesNames}, ${states}`)
+// })
+
+
+// const cities = (ArrayOfCity) =>{
+//     cities.forEach(city)
+// }
+/**
+ * Get Sum of People's Budget
+ * Create the function named getBudgets that takes an array with objects and returns the sum of people's budgets.
+ *
+ * Examples
+ *
+ * >> getBudgets([
+ *        { name: "John", age: 21, budget: 23000 },
+ *        { name: "Steve",  age: 32, budget: 40000 },
+ *        { name: "Martin",  age: 16, budget: 2700 }
+ *    ]) ➞ 65700
+ * >> getBudgets([
+ *        { name: "John",  age: 21, budget: 29000 },
+ *        { name: "Steve",  age: 32, budget: 32000 },
+ *        { name: "Martin",  age: 16, budget: 1600 }
+ *    ]) ➞ 62600
+ */
+
+//  const peopleAndBudgets = [
+//     { name: "John", age: 21, budget: 23000 },
+//     { name: "Steve",  age: 32, budget: 40000 },
+//     { name: "Martin",  age: 16, budget: 2700 }
+// ]
+// function getBudgets(arrayOfObjects){
+//      let totalBudget = 0;
+//      arrayOfObjects.forEach(object => totalBudget += object.budget);
+//      return totalBudget;
+// }
+// console.log(getBudgets(peopleAndBudgets));
