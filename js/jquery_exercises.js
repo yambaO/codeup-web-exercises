90f5675a1b55b46f46f66a92d86f1bb05e60eeb6
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-03-17 15:50:06 -0500

    Adding mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,33 +4,53 @@
 //
 
 // id Selector
-let contents = $('#title').html();
-alert(contents);
-$('#title').css('text-decoration','black  underline')
-
-//class Selector
-// $(".codeup").css('border', '1px solid red');
-
-$(".codeup").css({
-    'border': '1px solid red',
-    'width': 'fit-content',
-    'padding': '4px',
-    'border-radius': '5px',
-    'margin-bottom': '3px',
-    'background-color': 'gray',
-    'color': 'white'
-});
+// let contents = $('#title').html();
+// alert(contents);
+// $('#title').css('text-decoration','black  underline')
+//
+// //class Selector
+// // $(".codeup").css('border', '1px solid red');
+//
+// $(".codeup").css({
+//     'border': '1px solid red',
+//     'width': 'fit-content',
+//     'padding': '4px',
+//     'border-radius': '5px',
+//     'margin-bottom': '3px',
+//     'background-color': 'gray',
+//     'color': 'white'
+// });
 
 // Element Selectors
 
-$('li').css({
-    'font-size': '20px',
-    'color' : 'blue'
-});
+// $('li').css({
+//     'font-size': '20px',
+//     'color' : 'blue'
+// });
+//
+// // multiple selector
+//
+// $('*').css({
+//     'font-family' : 'Fantasy,sans-serif'
+// })
 
-// multiple selector
+$('#title').click(
+    function() {
+        $(this).css('background-color', '#FF0');
+    },
+);
 
-$('*').css({
-    'font-family' : 'Fantasy,sans-serif'
-})
+$('.codeup').dblclick(
+    function() {
+        $(this).css('font-size', '18px');
+    },
+);
 
+$('.shopping-item1').hover(
+    function() {
+        $(this).css('background-color', 'red');
+    },
+    function() {
+        $(this).css('background-color', '#FFF');
+    }
+);
