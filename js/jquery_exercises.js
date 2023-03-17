381561abb95534de633bd1a1e5375c1b8e9ce4fb
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-03-17 13:29:53 -0500

    Adding exercise jquery selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,36 @@
-window.onload = function() {
-    alert('The DOM has finished loading!');
-}
+// window.onload = function() {
+//     alert('The DOM has finished loading!');
+// }
+//
+
+// id Selector
+let contents = $('#title').html();
+alert(contents);
+$('#title').css('text-decoration','black  underline')
+
+//class Selector
+// $(".codeup").css('border', '1px solid red');
+
+$(".codeup").css({
+    'border': '1px solid red',
+    'width': 'fit-content',
+    'padding': '4px',
+    'border-radius': '5px',
+    'margin-bottom': '3px',
+    'background-color': 'gray',
+    'color': 'white'
+});
+
+// Element Selectors
+
+$('li').css({
+    'font-size': '20px',
+    'color' : 'blue'
+});
+
+// multiple selector
+
+$('*').css({
+    'font-family' : 'Fantasy,sans-serif'
+})
+
